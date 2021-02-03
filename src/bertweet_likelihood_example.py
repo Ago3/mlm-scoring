244f8b2e041e31c90afdfe016e3bc155b2555300
Ago3 <agostina.calabrese3@gmail.com>
2021-02-03 09:58:39 +0000

    Rename bertweet likelihood example

--- src/bertweet_likelihood_example.py
+++ src/bertweet_likelihood_example.py
@@ -34,13 +34,4 @@ print(tweetscorer.score_sentences([sentence]))
 
 print(tweetscorer.score_sentences([sentence], per_token=True))
 
-print('Done. Evaluating on waseem..')
-
-with open('../../waseem_abusive.tsv', 'r') as f:
-	abusive_scores = tweetscorer.score_sentences([line.strip() for line in f.readlines()])
-print('\nScore on abusive tweets: {}, {}, {}, {}\n'.format(np.mean(abusive_scores), np.std(abusive_scores), max(abusive_scores), min(abusive_scores)))
-with open('../../waseem_non_abusive.tsv', 'r') as f:
-	non_abusive_scores = tweetscorer.score_sentences([line.strip() for line in f.readlines()])
-print('\nScore on non abusive tweets: {}, {}, {}, {}\n'.format(np.mean(non_abusive_scores), np.std(abusive_scores), max(non_abusive_scores), min(non_abusive_scores)))
-
-print('\nDone!')
+print('Done.')
